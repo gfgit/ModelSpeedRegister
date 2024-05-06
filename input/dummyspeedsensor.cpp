126310b51a06a8ff2247bec342bfb43cf0bc2385
Filippo Gentile <filippogentile@disroot.org>
2024-05-06 18:42:49 +0200

    DummySpeedSensor: new dummy sensor with noise

---
+++ input/dummyspeedsensor.cpp
@@ -0,0 +1,64 @@
+#include "dummyspeedsensor.h"
+
+#include <QTimerEvent>
+#include <QRandomGenerator64>
+
+DummySpeedSensor::DummySpeedSensor(QObject *parent)
+    : ISpeedSensor{parent}
+{
+
+}
+
+DummySpeedSensor::~DummySpeedSensor()
+{
+    stop();
+}
+
+void DummySpeedSensor::timerEvent(QTimerEvent *e)
+{
+    if(e->timerId() == mTimerId)
+    {
+        double currentSpeed = mSpeedCurve.at(mCurrentStep);
+        const double MAX_OSCILLATION = 2.0;
+        double noise = QRandomGenerator::global()->bounded(MAX_OSCILLATION) - MAX_OSCILLATION / 2;
+        currentSpeed += noise;
+        emit speedReading(currentSpeed, LocomotiveDirection::Forward, mElapsedTimer.elapsed());
+        return;
+    }
+
+    ISpeedSensor::timerEvent(e);
+}
+
+void DummySpeedSensor::start()
+{
+    stop();
+    mTimerId = startTimer(1000);
+    mElapsedTimer.start();
+}
+
+void DummySpeedSensor::stop()
+{
+    if(mTimerId)
+    {
+        killTimer(mTimerId);
+        mTimerId = 0;
+    }
+}
+
+void DummySpeedSensor::simulateSpeedStep(int step)
+{
+    mCurrentStep = qBound(0, step, 126);
+}
+
+void DummySpeedSensor::generateRandomSpeedCurve()
+{
+    mSpeedCurve.resize(127);
+    mSpeedCurve[0] = 0;
+    const double MAX_INCREMENT = 5.0;
+
+    for(int i = 1; i < 127; i++)
+    {
+        double increment = QRandomGenerator::global()->bounded(MAX_INCREMENT);
+        mSpeedCurve[i] = mSpeedCurve[i - 1] + increment;
+    }
+}
---
+++ input/dummyspeedsensor.h
@@ -0,0 +1,33 @@
+#ifndef DUMMYSPEEDSENSOR_H
+#define DUMMYSPEEDSENSOR_H
+
+#include <QElapsedTimer>
+
+#include "ispeedsensor.h"
+
+class DummySpeedSensor : public ISpeedSensor
+{
+    Q_OBJECT
+public:
+    explicit DummySpeedSensor(QObject *parent = nullptr);
+    ~DummySpeedSensor();
+
+    void timerEvent(QTimerEvent *e) override;
+
+public slots:
+    void start();
+    void stop();
+    void simulateSpeedStep(int step);
+
+private:
+    void generateRandomSpeedCurve();
+
+private:
+    int mTimerId = 0;
+    QVector<double> mSpeedCurve;
+    int mCurrentStep = 0;
+
+    QElapsedTimer mElapsedTimer;
+};
+
+#endif // DUMMYSPEEDSENSOR_H
