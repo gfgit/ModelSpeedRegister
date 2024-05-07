39bb4350265451ac46b242ccbf709666f70df9e7
Filippo Gentile <filippogentile@disroot.org>
2024-05-07 12:52:28 +0200

    Add zoom and pan to charts

---
+++ chart/chartview.h
@@ -0,0 +1,34 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#ifndef CHARTVIEW_H
+#define CHARTVIEW_H
+
+#include <QChartView>
+#include <QRubberBand>
+
+//![1]
+class ChartView : public QChartView
+//![1]
+{
+    Q_OBJECT
+public:
+    ChartView(QChart *chart, QWidget *parent = nullptr);
+
+    //![2]
+protected:
+    bool viewportEvent(QEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    //![2]
+
+signals:
+    void scrollResetRequested();
+
+private:
+    bool m_isTouching = false;
+};
+
+#endif
