6f0cdc16519ac5fef10b5b2940cdae955dea0eff
Filippo Gentile <filippogentile@disroot.org>
2024-09-10 17:01:39 +0200

    RawSpeedCurveIO: load sparse curves

--- recorder/rawspeedcurveio.cpp
+++ recorder/rawspeedcurveio.cpp
@@ -16,23 +16,66 @@ bool RawSpeedCurveIO::readCurveFromFile(const QString &fileName, QLineSeries *se
     QJsonDocument doc = QJsonDocument::fromJson(f.readAll());
 
     QJsonObject obj = doc.object();
+
     QString name = obj.value(QLatin1String("name")).toString();
     if(name.isEmpty())
         name = f.fileName();
 
-    QJsonArray arr = obj.value(QLatin1String("curve_array")).toArray();
-    if(arr.size() != 127)
+    QJsonValue tmp = obj.value(QLatin1String("curve_array"));
+    if(tmp.isArray())
+    {
+        QJsonArray curve = tmp.toArray();
+        if(curve.size() != 127)
+            return false;
+
+        series->clear();
+
+        int step = 0;
+        for(auto val : curve)
+        {
+            series->append(step, val.toDouble());
+            step++;
+        }
+    }
+    else if(tmp = obj.value(QLatin1String("speed_mapping")); tmp.isArray())
+    {
+        series->clear();
+
+        QJsonArray sparseTable = tmp.toArray();
+
+        double lastSpeed = 0;
+        int lastStep = 0;
+
+        for(QJsonValue v : sparseTable)
+        {
+            QJsonObject stepObj = v.toObject();
+            int step = stepObj.value(QLatin1String("step")).toInt();
+            double speed = stepObj.value(QLatin1String("speed")).toDouble();
+
+            if(step > lastStep + 1)
+            {
+                // Linear interpolation of steps inbetween
+                int numSteps = step - lastStep;
+                double increment = (speed - lastSpeed) / double(numSteps);
+
+                for(int i = 1; i < numSteps; i++)
+                {
+                    double calculatedSpeed = lastSpeed + increment * double(i);
+                    int calculatedStep = lastStep + i;
+                    series->append(calculatedStep, calculatedSpeed);
+                }
+            }
+
+            lastStep = step;
+            lastSpeed = speed;
+
+            series->append(step, speed);
+        }
+    }
+    else
         return false;
 
     series->setName(name);
-    series->clear();
-
-    int step = 0;
-    for(auto val : arr)
-    {
-        series->append(step, val.toDouble());
-        step++;
-    }
 
     return true;
 }
